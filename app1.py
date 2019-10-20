763d261adbbe365ac5fe44202c9bdc34ca4608d1
Itanus <rob.cwi@hotmail.com>
2019-10-21 01:06:24 +0200

    New

--- app1.py
+++ app1.py
@@ -1,4 +1,6 @@
 import json
+import difflib
+from difflib import get_close_matches
 
 
 data = json.load(open("data.json"))
@@ -7,9 +9,11 @@ data = json.load(open("data.json"))
 def translation(word):
     if word.lower() in data:
         return data[word.lower()]
+    elif get_close_matches(word.lower(), data.keys())[0] in data:
+        return data[get_close_matches(word.lower(), data.keys())[0]]
     else:
-        return "This word does not exist." \
+        return "This word does not exist." 
 
 
-print(translation(  input("Type a word: ")))
+print(translation(input("Type a word: ")))
 
